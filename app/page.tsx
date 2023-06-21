0ef5f26e2c08c4214e8754849a44ca96cfbe8159
Ci3t <ranigfx@gmail.com>
2023-06-21 21:23:07 +0300

    Hero commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+
+import { Hero } from '@/component'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      
+      <Hero/>
     </main>
   )
 }
